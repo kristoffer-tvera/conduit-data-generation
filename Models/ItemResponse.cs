d2e629902b5cf20602a6eba66f3e71ba217ebc05
Kristoffer Tverå <Bluekristoffer@gmail.com>
2021-08-09 19:28:52 +0200

    Finished first draft, no pvp vendor atm

---
+++ Models/ItemResponse.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConduitData.Models
+{
+    class ItemResponse
+    {
+        [JsonPropertyName("_links")]
+        public Links Links { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("quality")]
+        public Quality Quality { get; set; }
+
+        [JsonPropertyName("level")]
+        public int Level { get; set; }
+
+        [JsonPropertyName("required_level")]
+        public int RequiredLevel { get; set; }
+
+        [JsonPropertyName("media")]
+        public Media Media { get; set; }
+
+        [JsonPropertyName("item_class")]
+        public ItemClass ItemClass { get; set; }
+
+        [JsonPropertyName("item_subclass")]
+        public ItemSubclass ItemSubclass { get; set; }
+
+        [JsonPropertyName("inventory_type")]
+        public InventoryType InventoryType { get; set; }
+
+        [JsonPropertyName("purchase_price")]
+        public int PurchasePrice { get; set; }
+
+        [JsonPropertyName("sell_price")]
+        public int SellPrice { get; set; }
+
+        [JsonPropertyName("max_count")]
+        public int MaxCount { get; set; }
+
+        [JsonPropertyName("is_equippable")]
+        public bool IsEquippable { get; set; }
+
+        [JsonPropertyName("is_stackable")]
+        public bool IsStackable { get; set; }
+
+        [JsonPropertyName("preview_item")]
+        public PreviewItem PreviewItem { get; set; }
+
+        [JsonPropertyName("purchase_quantity")]
+        public int PurchaseQuantity { get; set; }
+    }
+
+    public class Quality
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    public class Media
+    {
+        [JsonPropertyName("key")]
+        public Key Key { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+
+    public class ItemClass
+    {
+        [JsonPropertyName("key")]
+        public Key Key { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+
+    public class ItemSubclass
+    {
+        [JsonPropertyName("key")]
+        public Key Key { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+
+    public class InventoryType
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    public class Binding
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    public class Spell
+    {
+        [JsonPropertyName("spell")]
+        public Spell This { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+
+    public class Level
+    {
+        [JsonPropertyName("value")]
+        public int Value { get; set; }
+
+        [JsonPropertyName("display_string")]
+        public string DisplayString { get; set; }
+    }
+
+    public class PreviewItem
+    {
+        [JsonPropertyName("item")]
+        public Item Item { get; set; }
+
+        [JsonPropertyName("quality")]
+        public Quality Quality { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("media")]
+        public Media Media { get; set; }
+
+        [JsonPropertyName("item_class")]
+        public ItemClass ItemClass { get; set; }
+
+        [JsonPropertyName("item_subclass")]
+        public ItemSubclass ItemSubclass { get; set; }
+
+        [JsonPropertyName("inventory_type")]
+        public InventoryType InventoryType { get; set; }
+
+        [JsonPropertyName("binding")]
+        public Binding Binding { get; set; }
+
+        [JsonPropertyName("spells")]
+        public List<Spell> Spells { get; set; }
+
+        [JsonPropertyName("level")]
+        public Level Level { get; set; }
+
+        [JsonPropertyName("is_subclass_hidden")]
+        public bool IsSubclassHidden { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,13 @@ namespace ConduitData
 {
     class Program
     {
+        private static int CASTLE_NATHRIA_ID = 1190;
+        private static int SANCTUM_OF_DOMINATION_ID = 1193;
+        private static int WORLDBOSS_ID = 1192;
+        private static int SHADOWLANDS_ID = 499;
+        private static int TAZAVESH_ID = 1194;
+        private static string OUTPUT_FILENAME = "ConduitDB.lua";
+        private static int[] PVP_VENDOR = { 1, 2, 3, 4 }; //TODO: Add conduits from pvp vendor
         static async Task Main(string[] args)
         {
             var authToken = await GetAuthToken(GetConfig());
@@ -25,16 +32,20 @@ namespace ConduitData
             var conduitIndexResponse = await GetConduitIndexResponse(client);
 
             var conduits = new List<ConduitResponse>();
-            foreach(var conduit in conduitIndexResponse.Conduits)
+            foreach (var conduit in conduitIndexResponse.Conduits)
             {
                 conduits.Add(await GetConduitResponse(client, conduit));
             }
 
+            Console.WriteLine("Conduits fetched");
+
             var journalExpansionResponse = await GetJournalExpansionResponse(client);
             var journalExpansionWorldBossesInstanceResponse = await GetJournalInstanceResponseForWorldBosses(client);
 
+            Console.WriteLine("All dungeons / raids fetched");
+
             var journalEncounterDungeonResponse = new List<JournalInstanceResponse>();
-            foreach(var dungeon in journalExpansionResponse.Dungeons)
+            foreach (var dungeon in journalExpansionResponse.Dungeons)
             {
                 journalEncounterDungeonResponse.Add(await GetJournalInstanceResponse(client, dungeon.Key));
             }
@@ -45,50 +56,122 @@ namespace ConduitData
                 journalEncounterRaidResponse.Add(await GetJournalInstanceResponse(client, raid.Key));
             }
 
-
-            var pepe = GetJournalEncounterResponse(client, journalExpansionWorldBossesInstanceResponse.Encounters.First().Key);
-
-            //var encounters = new List<Encounter>();
-
-            //foreach(var raid in journalEncounterRaidResponse)
-            //{
-            //    encounters.AddRange(raid.Encounters);
-            //}
-
-            //foreach (var dungeon in journalEncounterDungeonResponse)
-            //{
-            //    encounters.AddRange(dungeon.Encounters);
-            //}
-
-            //encounters.AddRange(journalExpansionWorldBossesInstanceResponse.Encounters);
+            Console.WriteLine("All encounters from dungeons / raids fetched");
 
             var conduitWithDropLocations = new List<ConduitViewModel>();
             foreach (var dungeon in journalEncounterDungeonResponse)
             {
-                foreach(var encounterIdentifier in dungeon.Encounters)
+                foreach (var encounterIdentifier in dungeon.Encounters)
                 {
                     var encounter = await GetJournalEncounterResponse(client, encounterIdentifier.Key);
-                    foreach(var itemContainer in encounter.Items)
+                    foreach (var itemContainer in encounter.Items)
                     {
                         var conduit = conduits.FirstOrDefault(c => c.Name.EnGB == itemContainer.Item.Name.EnGB);
                         if (conduit == null) continue;
-                        conduitWithDropLocations.Add(new ConduitViewModel
+
+                        if (dungeon.Id == TAZAVESH_ID)
                         {
-                            Name = conduit.Name.EnGB,
-                            Ilvl145 = $"Dungeon ({encounter.Name.EnGB}), Normal",
-                            Ilvl158 = $"Dungeon ({encounter.Name.EnGB}), Heroic",
-                            Ilvl171 = $"Dungeon ({encounter.Name.EnGB}), Mythic",
-                        });
-
+                            conduitWithDropLocations.Add(new ConduitViewModel
+                            {
+                                Name = conduit.Name.EnGB,
+                                Ilvl226 = $"Dungeon ({dungeon.Name.EnGB})",
+                            });
+                        }
+                        else
+                        {
+                            conduitWithDropLocations.Add(new ConduitViewModel
+                            {
+                                Name = conduit.Name.EnGB,
+                                Ilvl158 = $"Dungeon ({dungeon.Name.EnGB})",
+                                Ilvl171 = $"Dungeon ({dungeon.Name.EnGB})",
+                                Ilvl184 = $"Dungeon ({dungeon.Name.EnGB})",
+                            });
+                        }
                     }
-
                 }
             }
 
+            Console.WriteLine("All data for dungeons complete");
+
+            foreach (var raid in journalEncounterRaidResponse)
+            {
+                foreach (var encounterIdentifier in raid.Encounters)
+                {
+                    var encounter = await GetJournalEncounterResponse(client, encounterIdentifier.Key);
+                    foreach (var itemContainer in encounter.Items)
+                    {
+                        var conduit = conduits.FirstOrDefault(c => c.Name.EnGB == itemContainer.Item.Name.EnGB);
+                        if (conduit == null) continue;
+
+                        if (raid.Id == CASTLE_NATHRIA_ID)
+                        {
+                            conduitWithDropLocations.Add(new ConduitViewModel
+                            {
+                                Name = conduit.Name.EnGB,
+                                Ilvl184 = $"Raid-boss ({encounter.Name.EnGB}), LFR",
+                                Ilvl200 = $"Raid-boss ({encounter.Name.EnGB}), Normal",
+                                Ilvl213 = $"Raid-boss ({encounter.Name.EnGB}), Heroic",
+                                Ilvl226 = $"Raid-boss ({encounter.Name.EnGB}), Mythic",
+                            });
+                        }
+
+                        if (raid.Id == SANCTUM_OF_DOMINATION_ID)
+                        {
+                            conduitWithDropLocations.Add(new ConduitViewModel
+                            {
+                                Name = conduit.Name.EnGB,
+                                Ilvl213 = $"Raid-boss ({encounter.Name.EnGB}), LFR",
+                                Ilvl226 = $"Raid-boss ({encounter.Name.EnGB}), Normal",
+                                Ilvl239 = $"Raid-boss ({encounter.Name.EnGB}), Heroic",
+                                Ilvl252 = $"Raid-boss ({encounter.Name.EnGB}), Mythic",
+                            });
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("All data for raids complete");
+
+            foreach (var worldbossIdentifier in journalExpansionWorldBossesInstanceResponse.Encounters)
+            {
+                var encounter = await GetJournalEncounterResponse(client, worldbossIdentifier.Key);
+                foreach (var itemContainer in encounter.Items)
+                {
+                    var conduit = conduits.FirstOrDefault(c => c.Name.EnGB == itemContainer.Item.Name.EnGB);
+                    if (conduit == null) continue;
+
+                    conduitWithDropLocations.Add(new ConduitViewModel
+                    {
+                        Name = conduit.Name.EnGB,
+                        Ilvl158 = $"World-quests",
+                        Ilvl171 = $"World-quests",
+                        Ilvl184 = $"World-quests",
+                        Ilvl200 = $"World-boss ({encounter.Name.EnGB})",
+                    });
+                }
+            }
+
+            Console.WriteLine("All data for worldbosses complete");
+
+            // Get item response to check vendor purchase flag?
+            //foreach(var conduit in conduits)
+            //{
+            //    var item = await GetItemResponse(client, conduit.Item.Id);
+            //}
+
+            conduitWithDropLocations = conduitWithDropLocations.OrderBy(c => c.Name).ToList();
+            var usedArrayKeys = new List<string>();
             var dropLocations = new List<string>();
             foreach (var conduit in conduitWithDropLocations)
             {
-                dropLocations.Add($"[\"{conduit.Name}\"] = {{[\"145\"] = \"{conduit.Ilvl145}\", [\"158\"] = \"{conduit.Ilvl158}\", [\"171\"] = \"{conduit.Ilvl171}\", [\"184\"] = \"{conduit.Ilvl184}\", [\"200\"] = \"{conduit.Ilvl200}\", [\"213\"] = \"{conduit.Ilvl213}\", [\"226\"] = \"{conduit.Ilvl226}\", [\"239\"] = \"{conduit.Ilvl239}\", [\"252\"] = \"{conduit.Ilvl252}\"}}");
+                var name = conduit.Name;
+                while (usedArrayKeys.Contains(name))
+                {
+                    name = $"_{name}";
+                }
+
+                dropLocations.Add($"[\"{name}\"] = {{[\"145\"] = \"{conduit.Ilvl145}\", [\"158\"] = \"{conduit.Ilvl158}\", [\"171\"] = \"{conduit.Ilvl171}\", [\"184\"] = \"{conduit.Ilvl184}\", [\"200\"] = \"{conduit.Ilvl200}\", [\"213\"] = \"{conduit.Ilvl213}\", [\"226\"] = \"{conduit.Ilvl226}\", [\"239\"] = \"{conduit.Ilvl239}\", [\"252\"] = \"{conduit.Ilvl252}\"}}");
+                usedArrayKeys.Add(name);
             }
 
 
@@ -97,10 +180,12 @@ namespace ConduitData
             sb.Append("addon.CONDUIT_DB = ");
             var allDropLocations = string.Join(",\r\n", dropLocations);
             sb.Append(allDropLocations);
+            sb.Append("};");
 
-            await System.IO.File.WriteAllTextAsync("ConduitDB.lua", sb.ToString());
+            await System.IO.File.WriteAllTextAsync(OUTPUT_FILENAME, sb.ToString());
 
-            Debugger.Break(); 
+            Console.WriteLine($"All data dumped to {OUTPUT_FILENAME}");
+
         }
 
         private static async Task<JournalInstanceResponse> GetJournalInstanceResponse(IRestClient restClient, Key key)
@@ -109,7 +194,7 @@ namespace ConduitData
 
             var request = new RestRequest(uri.PathAndQuery, Method.GET);
             var response = await restClient.ExecuteAsync<JournalInstanceResponse>(request);
-            
+
             if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
 
             return response.Data;
@@ -119,7 +204,7 @@ namespace ConduitData
         private static async Task<JournalInstanceResponse> GetJournalInstanceResponseForWorldBosses(IRestClient restClient)
         {
             // 1192 is the Shadowlands world bosses instance id
-            var request = new RestRequest("data/wow/journal-instance/1192");
+            var request = new RestRequest($"data/wow/journal-instance/{WORLDBOSS_ID}");
             request.AddParameter("namespace", "static-eu");
             var response = await restClient.ExecuteAsync<JournalInstanceResponse>(request);
 
@@ -136,16 +221,16 @@ namespace ConduitData
             var response = await restClient.ExecuteAsync<JournalEncounterResponse>(request);
 
             if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
-            
+
             return response.Data;
         }
 
         private static async Task<JournalExpansionResponse> GetJournalExpansionResponse(IRestClient restClient)
         {
-            var request = new RestRequest("/data/wow/journal-expansion/499", Method.GET);
+            var request = new RestRequest($"/data/wow/journal-expansion/{SHADOWLANDS_ID}", Method.GET);
             request.AddParameter("namespace", "static-eu");
             var response = await restClient.ExecuteAsync<JournalExpansionResponse>(request);
-            
+
             if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
 
             return response.Data;
@@ -156,7 +241,7 @@ namespace ConduitData
             var uri = new Uri(conduit.Key.Href);
             var request = new RestRequest(uri.PathAndQuery, Method.GET);
             var response = await restClient.ExecuteAsync<ConduitResponse>(request);
-            
+
             if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
 
             return response.Data;
@@ -167,7 +252,18 @@ namespace ConduitData
             var request = new RestRequest("/data/wow/covenant/conduit/index", Method.GET);
             request.AddParameter("namespace", "static-eu");
             var response = await restClient.ExecuteAsync<ConduitIndexResponse>(request);
-            
+
+            if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
+
+            return response.Data;
+        }
+
+        private static async Task<ItemResponse> GetItemResponse(IRestClient restClient, int itemId)
+        {
+            var request = new RestRequest($"/data/wow/item/{itemId}", Method.GET);
+            request.AddParameter("namespace", "static-eu");
+            var response = await restClient.ExecuteAsync<ItemResponse>(request);
+
             if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
 
             return response.Data;
@@ -181,7 +277,7 @@ namespace ConduitData
             var request = new RestRequest("/oauth/token", Method.POST);
             request.AddParameter("grant_type", "client_credentials");
             var response = await client.ExecuteAsync<AuthToken>(request);
-            
+
             if (!response.IsSuccessful && response.ErrorException != null) throw response.ErrorException;
 
             return response.Data;
